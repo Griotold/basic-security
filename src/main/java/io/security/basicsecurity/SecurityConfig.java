32531f0bdb1e251ba28a88d9524b5b2c639db5ff
Griotold <gotjd9773@naver.com>
2023-02-27 17:17:56 +0900

    권한 설정 및 표현식

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -2,41 +2,39 @@ package io.security.basicsecurity;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
+
+    // 사용자 만들기
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // {noop} : 암호화 알고리즘을 쓰지 않겠다. 평문으로 저장하겠다.
+        auth.inMemoryAuthentication().withUser("user").password("{noop}1111").roles("USER");
+        auth.inMemoryAuthentication().withUser("sys").password("{noop}1111").roles("SYS", "USER");
+        auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("ADMIN", "SYS", "USER");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // 인가 정책
         http
                 .authorizeRequests() // 요청에 대한 보안 검사
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/admin/pay").hasRole("ADMIN")
+                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
                 .anyRequest().authenticated(); // 어떤 요청에도 인증을 받음
-        // 인증 정책 : 폼 로그인 방식만 허용
+
         http
                 .formLogin();
 
-        // 세션 고정 보호 : 세션 고정 공격 방어 기능
-        http
-                .sessionManagement()
-                // .sessionFixation().none(); -> 세션 고정 공격에 속수무책으로 당한다.
-                .sessionFixation().changeSessionId(); // 인증 할 때마다 세션 ID가 바뀐다.
     }
 }
