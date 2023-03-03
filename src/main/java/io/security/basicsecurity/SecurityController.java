de3e1de846cc4775a9c46ab2f8473246ad7dffbc
Griotold <gotjd9773@naver.com>
2023-03-03 13:29:51 +0900

    3. 스프링 시큐리티 주요 아키텍쳐 이해 - 인증 저장소 - SecurityContextHolder, SecurityContext

--- src/main/java/io/security/basicsecurity/SecurityController.java
+++ src/main/java/io/security/basicsecurity/SecurityController.java
@@ -1,34 +1,41 @@
 package io.security.basicsecurity;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 public class SecurityController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(HttpSession session) {
+        // 아래 구문으로 어디서든지 인증 객체를 가져올 수 있다.
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        // 세션에서도 인증 객체를 가져올 수 있다. 위와 동일한 객체다.
+//        Object attribute = session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+        SecurityContext context = (SecurityContext) session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+        Authentication authentication1 = context.getAuthentication();
+
         return "home";
     }
 
-    @GetMapping("/user")
-    public String user() {
-        return "user";
+    @GetMapping("/thread")
+    public String thread() {
+        new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        // authentication = null
+                        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                    }
+                }
+        ).start();
+                return "thread";
     }
-
-    @GetMapping("/admin/pay")
-    public String adminPay() {
-        return "admin pay";
-    }
-
-    @GetMapping("/admin/**")
-    public String adminAll() {
-        return "admin all";
-    }
-
-    @GetMapping("/denied")
-    public String deny() { return "Access is denied"; }
-
-    @GetMapping("/login")
-    public String login() { return "login"; }
 }
