91352f5ebde3fec04eaf7d4fd132aeb2a77472b7
Griotold <gotjd9773@naver.com>
2023-03-17 13:19:05 +0900

    5) 인증 및 인가 예외 처리 - AjaxLoginUrlAuthenticationEntryPoint, AjaxAccessDeniedHandler

--- src/main/java/io/security/basicsecurity/controller/user/MessageController.java
+++ src/main/java/io/security/basicsecurity/controller/user/MessageController.java
@@ -2,6 +2,7 @@ package io.security.basicsecurity.controller.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MessageController {
@@ -9,5 +10,11 @@ public class MessageController {
     @GetMapping("/messages")
     public String message() throws Exception {
         return "/user/messages";
+    }
+
+    @GetMapping("/api/messages")
+    @ResponseBody // 응답 바디에 리턴값 실어서 사용자에게 응답
+    public String apiMessage() {
+        return "messages ok";
     }
 }
